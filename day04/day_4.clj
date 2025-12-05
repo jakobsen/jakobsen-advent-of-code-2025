8dd5a468e8ae324527ac06e3a24ff26d8649ccea
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-12-05 11:47:46 +0100

    Dag 4, nå med ny og bedre datastruktur

--- day04/day_4.clj
+++ day04/day_4.clj
@@ -1,39 +1,37 @@
 (ns day-4
   (:require [clojure.string :as str]))
 
-(defn build-board [raw-data]
-  (-> raw-data
-      (str/trim)
-      (str/split-lines)))
+(defn indexed [coll]
+  (map-indexed vector coll))
+
+(defn build-board [filename]
+  (into #{} (for [[i line] (indexed (str/split-lines (slurp filename)))
+                  [j char] (indexed line) :when (= char \@)]
+              [i j])))
 
 (defn get-all-coords [board]
   (for [i (range (count board)) j (range (count (first board)))] [i j]))
-
-(defn get-tile [board [i j]]
-  (-> board
-      (get i)
-      (get j)))
 
 (defn get-neighbour-coords [[i j]]
   (for [di [-1 0 1] dj [-1 0 1]
         :when (not= 0 di dj)]
     [(+ i di) (+ j dj)]))
 
-(defn is-paper? [board coords]
-  (= (get-tile board coords) \@))
+(defn count-neighbours [board coords]
+  (count (remove nil? (for [neighbour (get-neighbour-coords coords)] (board neighbour)))))
 
 (defn reachable? [board coords]
-  (and
-   (is-paper? board coords)
-   (<
-    (count
-     (filter
-      (fn [coords] (is-paper? board coords))
-      (get-neighbour-coords coords)))
-    4)))
+  (< (count-neighbours board coords) 4))
+
+(defn all-reachable [board]
+  (filter #(reachable? board %) board))
 
 (comment
-  (def test-board (build-board (slurp "test.txt")))
-  (def real-board (build-board (slurp "input.txt")))
+  (def test-board (build-board "test.txt"))
+  (def real-board (build-board "input.txt"))
 
-  (count (filter (fn [coords] (reachable? real-board coords)) (get-all-coords real-board))))
+  (count (all-reachable test-board))
+  ;; => 13
+  (count (all-reachable real-board))
+  ;; => 1349
+  )
