08e58c39b2ce86be758d177d82668881125714f3
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-12-06 15:25:57 +0100

    Dag 4 del 2: Fortsatt clojure

--- day04/day_4.clj
+++ day04/day_4.clj
@@ -1,5 +1,6 @@
 (ns day-4
-  (:require [clojure.string :as str]))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
 (defn indexed [coll]
   (map-indexed vector coll))
@@ -8,9 +9,6 @@
   (into #{} (for [[i line] (indexed (str/split-lines (slurp filename)))
                   [j char] (indexed line) :when (= char \@)]
               [i j])))
-
-(defn get-all-coords [board]
-  (for [i (range (count board)) j (range (count (first board)))] [i j]))
 
 (defn get-neighbour-coords [[i j]]
   (for [di [-1 0 1] dj [-1 0 1]
@@ -26,6 +24,13 @@
 (defn all-reachable [board]
   (filter #(reachable? board %) board))
 
+(defn clean-board [board]
+  (loop [current-board board]
+    (let [reachable (all-reachable current-board)]
+      (if (empty? reachable)
+        (- (count board) (count current-board))
+        (recur (set/difference current-board reachable))))))
+
 (comment
   (def test-board (build-board "test.txt"))
   (def real-board (build-board "input.txt"))
@@ -34,4 +39,10 @@
   ;; => 13
   (count (all-reachable real-board))
   ;; => 1349
+
+  ;; part 2:
+  (clean-board test-board)
+  ;; => 43
+  (clean-board real-board)
+  ;; => 8277
   )
